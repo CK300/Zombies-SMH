d094f6884ff406e80aec2b5adbdd60a143aefd65
CK300 <yanndeshpande@gmail.com>
2022-04-29 15:06:55 -0500

    Added gam and main

---
+++ src/Game.java
@@ -0,0 +1,166 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage; 
+import java.awt.event.*; 
+
+
+public class Game  extends JPanel implements Runnable, KeyListener, MouseListener, MouseMotionListener{
+
+	
+	private BufferedImage back; 
+	private int key,  x, y;
+	private ImageIcon lucki, background;
+	
+	
+	
+	public Game() {
+		new Thread(this).start();	
+		this.addKeyListener(this);
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		key =-1; 
+		x=0;
+		y=0;
+	}
+
+	
+	
+	public void run()
+	   {
+	   	try
+	   	{
+	   		while(true)
+	   		{
+	   		   Thread.currentThread().sleep(5);
+	            repaint();
+	         }
+	      }
+	   		catch(Exception e)
+	      {
+	      }
+	  	}
+	
+
+	
+	
+	
+	public void paint(Graphics g){
+		
+		Graphics2D twoDgraph = (Graphics2D) g; 
+		if( back ==null)
+			back=(BufferedImage)( (createImage(getWidth(), getHeight()))); 
+		
+
+		Graphics g2d = back.createGraphics();
+	
+		g2d.clearRect(0,0,getSize().width, getSize().height);
+		background = new ImageIcon("Studio.jpg");
+		g2d.drawImage(background.getImage(),0,0,getWidth(),getHeight(),this);
+		lucki = new ImageIcon("Luckibg.png");
+		g2d.drawImage(lucki.getImage(),x,y,getWidth(),getHeight(),this);
+		g2d.setFont(new Font("BROADWAY",Font.PLAIN, 50));
+		g2d.setColor(Color.WHITE);
+		g2d.drawString("Lucki the GOAT",x,y);
+
+		twoDgraph.drawImage(back, null, 0, 0);
+
+	}
+
+	
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		key= e.getKeyCode();
+		System.out.println(key);
+		
+		
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	
+		
+		
+		
+	}
+
+
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub\\
+		
+		
+	}
+
+
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		x=arg0.getX();
+		y=arg0.getY();
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+	
+}
